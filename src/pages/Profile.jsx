d5deff2e9c261e8ad0120dee0599b66b7335c812
Imaadh Rushdee <rushdeeimaadh@gmail.com>
2026-01-03 11:17:59 +0530

    Scroll effect

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,14 +10,14 @@ import AnimatedSection from "../components/AnimatedSection"
 function Dashboard() {
     return (
         <>
-                <AnimatedSection><Hero /></AnimatedSection>
-                <AnimatedSection><Bio /></AnimatedSection>
-                <AnimatedSection><Skills /></AnimatedSection>
-                <AnimatedSection><Projects /></AnimatedSection>
-                <AnimatedSection><Education /></AnimatedSection>
-                <AnimatedSection><References /></AnimatedSection>
-                <AnimatedSection><Contact /></AnimatedSection>
-                <AnimatedSection><Footer /></AnimatedSection>
+                <AnimatedSection><Hero />
+                <Bio />
+                <Skills />
+                <Projects />
+                <Education />
+                <References />
+                <Contact />
+                <Footer /></AnimatedSection>
         </>
     )
 }
